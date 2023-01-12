286f1a8d11dadfd40c2ea74f6cac565270682527
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2023-01-12 16:06:34 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly Student =new RWOnly();
+    Student.setName("MD Mubarak");
+    Student.getName();
+  }
 }
