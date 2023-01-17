436a46bb005213d9caddf70780ce603e5d662941
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2023-01-17 15:55:48 +0530

    done updatation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
     RWOnly Student =new RWOnly();
+    Student.name="Md Mubarak";
+    //'name' has private access in 'com.driver.RWOnly'
     Student.setName("MD Mubarak");
     Student.getName();
   }
